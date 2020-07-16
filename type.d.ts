9c01d91cbb695fbf5fc62087b5f4e0c0dde16dc5
tingyun <tingyun@pinduoduo.com>
2020-07-16 14:18:58 +0800

    Feat: add prettierrc、tsconfig

---
+++ type.d.ts
@@ -0,0 +1,17 @@
+declare module '*.scss' {
+  const content: { [className: string]: string };
+  export = content;
+}
+
+declare module '*.module.scss' {
+  const content: { [className: string]: string };
+  export = content;
+}
+
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.bmp';
+declare module '*.tiff';
