a961b6e8083b7e6aa8ec20ece4a33ac214a79575
tingyun <tingyun@pinduoduo.com>
2020-07-16 14:19:55 +0800

    Fix: fix main process

--- src/main/index.js
+++ src/main/index.js
@@ -1,34 +1,86 @@
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, globalShortcut } from 'electron';
+import path from 'path';
 
-let mainWindow = null
+let isProd = process.env.NODE_ENV === 'production';
+let mainWindow = null;
+let port = process.env.DEV_PORT || 8080;
+let isQuiting;
 
-let port = process.env.DEV_PORT || 8080
-
-// console.log(port)
-
-app.on('ready', () => {
+const createWindow = () => {
   mainWindow = new BrowserWindow({
-    width: 1024,
+    width: 1096,
     height: 728,
-    show: false
-  })
+    show: false,
+    webPreferences: !isProd
+      ? {
+          nodeIntegration: true,
+        }
+      : {
+          preload: path.join(__dirname, 'renderer.js'),
+        },
+  });
 
-  const winURL =
-    process.env.NODE_ENV === 'production'
-      ? `file://${__dirname}/index.html`
-      : `http://localhost:${port}`
-  mainWindow.loadURL(winURL)
+  const winURL = isProd ? `file://${__dirname}/index.html` : `http://localhost:${port}`;
 
+  mainWindow.loadURL(winURL);
   mainWindow.webContents.on('did-finish-load', () => {
     if (!mainWindow) {
-      throw new Error('"mainWindow" is not defined')
+      throw new Error('"mainWindow" is not defined');
     }
 
-    mainWindow.show()
-    mainWindow.focus()
-  })
+    mainWindow.show();
+    mainWindow.focus();
+  });
 
-  mainWindow.on('close', () => (mainWindow = null))
-})
+  mainWindow.on('minimize', function (event) {
+    event.preventDefault();
+    mainWindow.hide();
+  });
 
-app.on('window-all-close', () => process.platform !== 'darwin' && app.quit())
+  mainWindow.on('close', function (event) {
+    if (isProd) {
+      if (!isQuiting) {
+        event.preventDefault();
+        mainWindow.hide();
+        return false;
+      }
+    } else {
+      mainWindow = null;
+    }
+  });
+};
+
+app.on('before-quit', function () {
+  isQuiting = true;
+});
+
+app.on('activate', () => {
+  if (mainWindow) {
+    mainWindow.show();
+  } else {
+    createWindow();
+  }
+});
+
+app.on('ready', () => {
+  // 生产模式，用于调试
+  globalShortcut.register('CommandOrControl+Shift+L', () => {
+    let focusWin = BrowserWindow.getFocusedWindow();
+    focusWin && focusWin.toggleDevTools();
+  });
+
+  createWindow();
+});
+
+app.on('window-all-close', () => process.platform !== 'darwin' && app.quit());
+
+const shouldQuit = app.makeSingleInstance((commandLine, workingDir) => {
+  if (mainWindow) {
+    mainWindow.isMinimized() && mainWindow.restore();
+    mainWindow.focus();
+  }
+});
+
+if (shouldQuit) {
+  app.quit();
+}
